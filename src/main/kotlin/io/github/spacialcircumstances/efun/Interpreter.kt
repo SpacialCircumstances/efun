1389b614ddd9dbaca277609f6569fd43a20f0920
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-09 17:59:55 +0200

    Begin scanner

--- src/main/kotlin/io/github/spacialcircumstances/efun/Interpreter.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/Interpreter.kt
@@ -1,10 +1,9 @@
 package io.github.spacialcircumstances.efun
 
 class Interpreter {
-    val scanner = Scanner()
     val parser = Parser()
 
     fun interpret(code: String) {
-        val tokens = scanner.tokenize(code)
+        val tokens = tokenize(code)
     }
 }
--- src/main/kotlin/io/github/spacialcircumstances/efun/Scanner.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/Scanner.kt
@@ -1,7 +1,12 @@
 package io.github.spacialcircumstances.efun
 
-class Scanner {
-    fun tokenize(code: String): List<Token> {
-        return emptyList()
-    }
+fun tokenize(code: String): List<Token> {
+    val state = ScannerState(listOf(), code, 0, 0, 1)
+    return state.tokens
 }
+
+private fun isAtEnd(state: ScannerState): Boolean {
+    return state.current >= state.source.length
+}
+
+data class ScannerState(val tokens: List<Token>, val source: String, val start: Int, val current: Int, val line: Int)
