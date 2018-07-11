040f44a3d0aa88bc4b982ba23ed91213ef2a631c
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-11 18:19:03 +0200

    Use arbitrary matcher function

--- src/main/kotlin/io/github/spacialcircumstances/efun/parser/Parser.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/parser/Parser.kt
@@ -4,12 +4,12 @@ class Parser<T, out R>(private val parseFunction: (List<T>) -> Pair<List<R>?, Li
     fun run(input: List<T>): Pair<List<R>?, List<T>> = parseFunction(input)
 }
 
-fun<T, R> one(match: T, convert: (T) -> R): Parser<T, R> {
+fun<T, R> one(match: (T) -> Boolean, convert: (T) -> R): Parser<T, R> {
     return Parser({ input ->
         if (input.isEmpty()) Pair(null, input) else {
             val first = input.first()
             val rest = if (input.size > 1) input.subList(1, input.size) else emptyList()
-            if (first == match) {
+            if (match(first)) {
                 val result = convert(first)
                 Pair(listOf(result), rest)
             } else {
