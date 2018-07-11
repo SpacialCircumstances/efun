047b5dc9437d2d3194692ebde009eaa7e5e4010b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-11 16:46:20 +0200

    Add parser type

--- src/main/kotlin/io/github/spacialcircumstances/efun/parser/Parser.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/parser/Parser.kt
@@ -1,15 +1,22 @@
 package io.github.spacialcircumstances.efun.parser
 
-fun<T, R> one(input: List<T>, match: T, convert: (T) -> R): Pair<List<R>?, List<T>> {
-    if (input.isEmpty()) return Pair(null, input)
-    val first = input.first()
-    val rest = if (input.size > 1) input.subList(1, input.size) else emptyList()
-    return if (first == match) {
-        val result = convert(first)
-        Pair(listOf(result), rest)
-    } else {
-        Pair(null, rest)
-    }
+class Parser<T, out R>(private val parseFunction: (List<T>) -> Pair<List<R>?, List<T>>) {
+    fun run(input: List<T>): Pair<List<R>?, List<T>> = parseFunction(input)
+}
+
+fun<T, R> one(match: T, convert: (T) -> R): Parser<T, R> {
+    return Parser({ input ->
+        if (input.isEmpty()) Pair(null, input) else {
+            val first = input.first()
+            val rest = if (input.size > 1) input.subList(1, input.size) else emptyList()
+            if (first == match) {
+                val result = convert(first)
+                Pair(listOf(result), rest)
+            } else {
+                Pair(null, rest)
+            }
+        }
+    })
 }
 
 fun<T, R> orElse(first: (List<T>) -> Pair<List<R>?, List<T>>, second: (List<T>) -> Pair<List<R>?, List<T>>): (List<T>) -> Pair<List<R>?, List<T>> {
