b2f17de0f19ef1e6bd935b495d17b2999ae7435e
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-13 19:40:49 +0200

    Work on parser

--- src/main/kotlin/io/github/spacialcircumstances/efun/Parser.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/Parser.kt
@@ -1,5 +1,6 @@
 package io.github.spacialcircumstances.efun
 
+import io.github.spacialcircumstances.efun.expressions.AbstractExpression
 import io.github.spacialcircumstances.efun.expressions.LiteralExpression
 import io.github.spacialcircumstances.efun.interpreter.FValue
 import io.github.spacialcircumstances.efun.interpreter.FValueType
@@ -25,8 +26,20 @@ val falseLiteralParser = oneWith<LiteralExpression, Token>({ it.type == TokenTyp
     LiteralExpression(FValue(FValueType.Bool, false))
 }
 
+val expressionParser = lazy { createExpressionParser() }
+
 val literalParser = choice(stringLiteralParser, intLiteralParser, floatLiteralParser, trueLiteralParser, falseLiteralParser)
 
 val arrowParser = one<Token> { it.type == TokenType.ARROW }
 
-val letAndEqualParser = one<Token> { it.type == TokenType.LET }.andThen(one<Token> { it.type == TokenType.EQUAL })
+val nameParser = oneWith<String, Token>({ it.type == TokenType.IDENTIFIER }) {
+    it.literal as String
+}
+
+val letNameParser = takeMiddle(one { it.type == TokenType.LET },
+                                nameParser,
+                                one { it.type == TokenType.EQUAL })
+
+fun createExpressionParser(): Parser<AbstractExpression, Token> {
+    return choice(literalParser)
+}
