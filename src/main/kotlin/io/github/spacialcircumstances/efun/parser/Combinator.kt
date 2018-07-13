eb23f809cb348999c394bb020e51def3e1a55351
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-13 16:13:46 +0200

    Add andThen combinator

--- src/main/kotlin/io/github/spacialcircumstances/efun/parser/Combinator.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/parser/Combinator.kt
@@ -1,2 +1,16 @@
 package io.github.spacialcircumstances.efun.parser
 
+fun<R1, R2, T> Parser<R1, T>.andThen(p2: Parser<R2, T>): Parser<Pair<R1, R2>, T> =
+    Parser { input ->
+        val (result, rem) = this.run(input)
+        if (result == null) {
+            Pair(null, input)
+        } else {
+            val (secondResult, rem2) = p2.run(rem)
+            if (secondResult == null) {
+                Pair(null, input)
+            } else {
+                Pair(Pair(result, secondResult), rem2)
+            }
+        }
+    }
