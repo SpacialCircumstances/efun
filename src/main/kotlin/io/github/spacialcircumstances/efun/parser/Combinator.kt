642777fccfc62d1004fbc03d5c24d8e98c9711df
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-13 17:36:45 +0200

    Add sequence combinator

--- src/main/kotlin/io/github/spacialcircumstances/efun/parser/Combinator.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/parser/Combinator.kt
@@ -31,3 +31,25 @@ fun<R, T> choice(vararg parsers: Parser<R, T>): Parser<R, T> {
         p1.orElse(p2)
     }
 }
+
+fun<R, T> sequence(vararg parsers: Parser<R, T>): Parser<List<R>, T> {
+    return Parser {
+        if (it.isEmpty()) {
+            Pair(emptyList<R>(), it)
+        }
+        val results: MutableList<R> = mutableListOf()
+        var success = true
+        var current = it
+        for (parser in parsers) {
+            val (r, rem) = parser.run(current)
+            if (r == null) {
+                success = false
+                break
+            } else {
+                results.add(r)
+                current = rem
+            }
+        }
+        if (success) Pair(results, current) else Pair(null, it)
+    }
+}
