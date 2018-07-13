e6924a3f45086c8b5ad37633225517d0cb446707
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-13 19:04:46 +0200

    Add literal parsers

--- src/main/kotlin/io/github/spacialcircumstances/efun/Parser.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/Parser.kt
@@ -1,2 +1,32 @@
 package io.github.spacialcircumstances.efun
 
+import io.github.spacialcircumstances.efun.expressions.LiteralExpression
+import io.github.spacialcircumstances.efun.interpreter.FValue
+import io.github.spacialcircumstances.efun.interpreter.FValueType
+import io.github.spacialcircumstances.efun.parser.*
+
+val stringLiteralParser = oneWith<LiteralExpression, Token>({ it.type == TokenType.STRING }) {
+    LiteralExpression(FValue(FValueType.String, it.literal))
+}
+
+val intLiteralParser = oneWith<LiteralExpression, Token>({ it.type == TokenType.INTEGER }) {
+    LiteralExpression(FValue(FValueType.Int, it.literal))
+}
+
+val floatLiteralParser = oneWith<LiteralExpression, Token>({ it.type == TokenType.FLOAT }) {
+    LiteralExpression(FValue(FValueType.Float, it.literal))
+}
+
+val trueLiteralParser = oneWith<LiteralExpression, Token>({ it.type == TokenType.TRUE }) {
+    LiteralExpression(FValue(FValueType.Bool, true))
+}
+
+val falseLiteralParser = oneWith<LiteralExpression, Token>({ it.type == TokenType.FALSE }) {
+    LiteralExpression(FValue(FValueType.Bool, false))
+}
+
+val literalParser = choice(stringLiteralParser, intLiteralParser, floatLiteralParser, trueLiteralParser, falseLiteralParser)
+
+val arrowParser = one<Token> { it.type == TokenType.ARROW }
+
+val letAndEqualParser = one<Token> { it.type == TokenType.LET }.andThen(one<Token> { it.type == TokenType.EQUAL })
