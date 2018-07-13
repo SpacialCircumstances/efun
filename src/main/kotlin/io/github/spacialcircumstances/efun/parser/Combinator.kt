59f048be1116cdca6ae96c08d31fac7fb7faccf9
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-13 16:24:05 +0200

    Add orElse combinator

--- src/main/kotlin/io/github/spacialcircumstances/efun/parser/Combinator.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/parser/Combinator.kt
@@ -14,3 +14,14 @@ fun<R1, R2, T> Parser<R1, T>.andThen(p2: Parser<R2, T>): Parser<Pair<R1, R2>, T>
             }
         }
     }
+
+fun<R, T> Parser<R, T>.orElse(other: Parser<R, T>): Parser<R, T> =
+    Parser { input ->
+        val (result, rem) = this.run(input)
+        if (result == null) {
+            val (secondResult, rem2) = other.run(input)
+            Pair(secondResult, rem2)
+        } else {
+            Pair(result, rem)
+        }
+    }
