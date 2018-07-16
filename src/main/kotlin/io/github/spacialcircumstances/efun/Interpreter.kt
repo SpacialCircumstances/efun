a719cd164a0fcacfcdc6d4dd5d84373f95b62e6a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-16 20:21:49 +0200

    Add interpreter & context

--- src/main/kotlin/io/github/spacialcircumstances/efun/Interpreter.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/Interpreter.kt
@@ -1,11 +1,17 @@
 package io.github.spacialcircumstances.efun
 
+import io.github.spacialcircumstances.efun.interpreter.InterpreterContext
+
 class Interpreter {
+    val context = InterpreterContext(null)
+
     fun interpret(code: String) {
         val tokens = tokenize(code)
         println(tokens.joinToString { it.type.toString() })
         val parseResult = programParser.run(tokens)
         val ast = parseResult.first
-        println(ast?.size)
+        ast?.forEach {
+            it.evaluate(context)
+        }
     }
 }
--- src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/InterpreterContext.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/InterpreterContext.kt
@@ -1,4 +1,16 @@
 package io.github.spacialcircumstances.efun.interpreter
 
-class InterpreterContext {
+class InterpreterContext(val parent: InterpreterContext?) {
+    private val variables = mutableMapOf<String, FValue>()
+    operator fun get(key: String): FValue? {
+        return variables[key] ?: parent?.get(key)
+    }
+
+    operator fun set(key: String, variable: FValue) {
+        if (variables[key] != null) {
+            throw IllegalStateException("Values must be immutable")
+        } else {
+            variables[key] = variable
+        }
+    }
 }
