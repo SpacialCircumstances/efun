1e4fc5da6897dc695dfc67106e6afc9ab359a178
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-07-20 18:30:39 +0200

    Simple function currying

--- src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/FFunction.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/FFunction.kt
@@ -4,7 +4,8 @@ import io.github.spacialcircumstances.efun.expressions.AbstractExpression
 
 class FFunction(val parameters: List<Pair<String, FType<*>>>, val expressions: List<AbstractExpression>, val environment: InterpreterContext) {
     fun run(arguments: List<FValue>): FValue {
-        if (arguments.size != parameters.size) throw IllegalStateException("Arguments count of ${arguments.size} does not match intended of ${parameters.size}")
+        if (arguments.size > parameters.size) throw IllegalStateException("Arguments count of ${arguments.size} does not match intended of ${parameters.size}")
+        if (arguments.size < parameters.size) return curried(arguments)
         val newEnv = environment.copy()
         parameters.forEachIndexed { index, s ->
             val arg = arguments[index]
@@ -12,5 +13,16 @@ class FFunction(val parameters: List<Pair<String, FType<*>>>, val expressions: L
             newEnv[s.first] = arg
         }
         return expressions.map { it.evaluate(newEnv) }.last()
+    }
+
+    private fun curried(arguments: List<FValue>): FValue {
+        val curryEnv = environment.copy()
+        arguments.forEachIndexed { index, fValue ->
+            val parameter = parameters[index]
+            if (parameter.second != fValue.type) throw IllegalStateException("Type ${parameter.second} expected as parameter $index, but ${fValue.type} given")
+            curryEnv[parameter.first] = fValue
+        }
+        val restParams = parameters.subList(arguments.size, parameters.size)
+        return FValue(TFunction, FFunction(restParams, expressions, curryEnv))
     }
 }
