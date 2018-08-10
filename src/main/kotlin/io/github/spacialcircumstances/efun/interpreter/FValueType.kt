55988cb402d8785452361b5a8ec9041e953e7d68
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-08-10 22:40:36 +0200

    Add function type

--- src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/FValueType.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/FValueType.kt
@@ -16,6 +16,7 @@ abstract class FType<out T> {
 
 class SimpleType<out T>(override val name: String): FType<T>() {
     override fun castValue(value: FValue): T {
+        @Suppress("UNCHECKED_CAST")
         return value.value as T
     }
 
@@ -49,6 +50,20 @@ class AnyType: FType<Any>() {
         return value.type.castValue(value)!!
     }
 
+}
+
+class FunctionType(val inType: FType<*>, val outType: FType<*>): FType<FunctionPointer>() {
+    override val name: String = "(${inType.name} -> ${outType.name})"
+
+    override fun castValue(value: FValue): FunctionPointer {
+        return value.value as FunctionPointer
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is FunctionType) {
+            (other.inType == inType) && (other.outType == outType)
+        } else false
+    }
 }
 
 val TVoid = VoidType()
