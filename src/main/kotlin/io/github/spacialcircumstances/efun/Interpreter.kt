3d8206098efdbf5e0d11a4920af5bfce47602be4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-08-13 14:46:24 +0200

    Very first type checking in the interpreter!

--- src/main/kotlin/io/github/spacialcircumstances/efun/Interpreter.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/Interpreter.kt
@@ -2,9 +2,11 @@ package io.github.spacialcircumstances.efun
 
 import io.github.spacialcircumstances.efun.interpreter.FValue
 import io.github.spacialcircumstances.efun.interpreter.InterpreterContext
+import io.github.spacialcircumstances.efun.interpreter.TypeContext
 
 class Interpreter {
     val context = InterpreterContext(null)
+    val tc = TypeContext(null)
 
     fun interpret(code: String, resultCallback: (FValue) -> Unit = {}) {
         val tokens = tokenize(code)
@@ -14,6 +16,9 @@ class Interpreter {
             val firstUnparsedToken = parseResult.second.first()
             println("Parser error in line: ${firstUnparsedToken.line} with Token: ${firstUnparsedToken.type}")
         } else {
+            ast?.forEach {
+                it.guessType(tc)
+            }
             ast?.forEach {
                 resultCallback(it.evaluate(context))
             }
