f4b6078b020b05ed5863af845d1fbaec4ec52786
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-08-16 15:23:29 +0200

    External functions with 2 parameters

--- src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/ExternalFunction.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/ExternalFunction.kt
@@ -15,3 +15,15 @@ class ExternalFunction1<P1, R>(val external: (P1) -> R, override val functionTyp
         return FValue(functionType.outType, result)
     }
 }
+
+class ExternalFunction2<P1, P2, R>(val external: (P1, P2) -> R, override val functionType: FunctionType): ExternalFunction() {
+    override val argumentCount: Int = 2
+
+    override fun run(args: List<FValue>): FValue {
+        val nativeArg1 = args.first().value as P1
+        val nativeArg2 = args[1].value as P2
+        val result = external(nativeArg1, nativeArg2)
+        val out = (functionType.outType as FunctionType).outType
+        return FValue(out, result)
+    }
+}
