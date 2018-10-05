19403a8d6e17864acac32e64c54e2d971a7d1ba5
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-10-05 18:34:35 +0200

    Add module value and type stores

--- src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/Module.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/Module.kt
@@ -1,7 +1,5 @@
 package io.github.spacialcircumstances.efun.interpreter
 
-class Module: IFTypeStore {
-    override fun getType(key: String): FType<*>? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class Module(val typeContext: TypeContext): IFTypeStore {
+    override fun getType(key: String): FType<*>? = typeContext.getType(key)
 }
