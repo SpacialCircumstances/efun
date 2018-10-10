7cf69419d0095528fa67b122ffb55aaf2e4b579b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-10-10 21:23:02 +0200

    Abstract type expression for expressions that create types

---
+++ src/main/kotlin/io/github/spacialcircumstances/efun/expressions/AbstractTypeExpression.kt
@@ -0,0 +1,11 @@
+package io.github.spacialcircumstances.efun.expressions
+
+import io.github.spacialcircumstances.efun.interpreter.DataStructureType
+import io.github.spacialcircumstances.efun.interpreter.FValue
+import io.github.spacialcircumstances.efun.interpreter.InterpreterContext
+import io.github.spacialcircumstances.efun.interpreter.TypesContext
+
+abstract class AbstractTypeExpression {
+    abstract fun evaluate(context: InterpreterContext): FValue
+    abstract fun type(context: TypesContext): DataStructureType
+}
