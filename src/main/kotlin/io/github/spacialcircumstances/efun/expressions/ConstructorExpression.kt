582d19cd2d06d2301b1d05fa08096a31b1b2fa8e
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-10-10 21:45:20 +0200

    Implement record constructors based on Datastructure

--- src/main/kotlin/io/github/spacialcircumstances/efun/expressions/ConstructorExpression.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/expressions/ConstructorExpression.kt
@@ -4,20 +4,25 @@ import io.github.spacialcircumstances.efun.TypeError
 import io.github.spacialcircumstances.efun.interpreter.*
 
 class ConstructorExpression(private val typeName: PlaceholderType, val fields: List<Pair<String, AbstractExpression>>): AbstractExpression() {
-    var type: RecordType? = null
+    var type: DataStructureType? = null
 
     override fun evaluate(context: InterpreterContext): FValue {
         val params = fields.map { Pair(it.first, it.second.evaluate(context)) }.toMap()
-        val instance = type!!.definition.createInstance(params)
+        val dsContext = InterpreterContext(null)
+        params.forEach { name, value ->
+            dsContext[name] = value
+        }
+        val instance = DataStructureInstance(dsContext)
         return FValue(type!!, instance)
     }
 
     override fun guessType(context: TypesContext): FType<*> {
-        type = typeName.resolveType(context) as RecordType
-        if (type == null) throw TypeError("$typeName is not a record type")
+        type = typeName.resolveType(context) as DataStructureType
+        if (type == null) throw TypeError("$typeName is not a data structure type")
         val params = fields.map { Pair(it.first, it.second.guessType(context)) }.toMap()
-        if (!type!!.definition.checkValues(params)) {
-            throw TypeError("Error in constructor for type ${type!!.name}")
+        val dataStructureContext = type!!.definition.typesContext
+        params.forEach { name, t ->
+            if (dataStructureContext.getType(name) != t) throw TypeError("Constructor type for $name does not match expected type $t")
         }
         return type!!
     }
