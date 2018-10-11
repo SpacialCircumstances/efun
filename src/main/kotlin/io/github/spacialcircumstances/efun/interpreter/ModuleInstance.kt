f7fe201425652b31e0d6d7d4bcdee8d753b0070f
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-10-11 21:55:29 +0200

    Remove old module code

--- src/main/kotlin/io/github/spacialcircumstances/efun/interpreter/ModuleInstance.kt
+++
@@ -1,5 +0,0 @@
-package io.github.spacialcircumstances.efun.interpreter
-
-class ModuleInstance(val interpreterContext: InterpreterContext): IFValueStore {
-    override fun get(key: String): FValue? = interpreterContext[key]
-}
