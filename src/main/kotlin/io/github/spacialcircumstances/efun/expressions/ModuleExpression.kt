a5e0dd2d5356a77f5854a58d6d849f2174c3068b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-10-11 21:48:53 +0200

    Implement modules based on Datastructure system

--- src/main/kotlin/io/github/spacialcircumstances/efun/expressions/ModuleExpression.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/expressions/ModuleExpression.kt
@@ -3,11 +3,33 @@ package io.github.spacialcircumstances.efun.expressions
 import io.github.spacialcircumstances.efun.interpreter.*
 
 class ModuleExpression(val name: String, val uses: List<String>, val expressions: List<AbstractExpression>): AbstractTypeExpression() {
+    var type: DataStructureType? = null
+
     override fun evaluate(context: InterpreterContext): FValue {
-        TODO()
+        val moduleContext = InterpreterContext(null)
+        uses.forEach {
+            moduleContext.importExternModule(it, context[it]!!)
+        }
+        expressions.forEach {
+            it.evaluate(moduleContext)
+        }
+        val instance = DataStructureInstance(moduleContext)
+        val value = FValue(type!!, instance)
+        context[name] = value
+        return value
     }
 
     override fun type(context: TypesContext): DataStructureType {
-        TODO()
+        val moduleContext = TypesContext(null, context.defaultTypeMappings)
+        uses.forEach {
+            val mod = (context.getType(it) as TypeType).type as DataStructureType
+            moduleContext.importExternModule(mod)
+        }
+        expressions.forEach {
+            it.guessType(moduleContext)
+        }
+        val dataStructureDefinition = DataStructureDefinition(moduleContext)
+        type = DataStructureType(name, dataStructureDefinition)
+        return type!!
     }
 }
