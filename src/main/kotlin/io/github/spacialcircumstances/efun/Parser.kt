5e7ce82a7cf59fad54c9044a7dace6073cb86c97
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-10-16 20:33:40 +0200

    Create runtime types for objects and creating them

--- src/main/kotlin/io/github/spacialcircumstances/efun/Parser.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/Parser.kt
@@ -135,7 +135,7 @@ val objectParametersParser = takeMiddle(openParensParser, (singleParamParser.sep
 val bodyParser = takeMiddle(leftBraceParser, expressionParser.many(), rightBraceParser)
 
 val objectDefinitionParser = typeExprNameParser.andIgnoreResult(one { it.type == TokenType.OBJECT }).andThen(objectParametersParser).andThen(bodyParser).map {
-    TypeExpression(it.first.first, ObjectTypeExpression(it.first.second.singleOrNull()?.toMap() ?: emptyMap(), it.second))
+    TypeExpression(it.first.first, ObjectTypeExpression(it.first.first, it.first.second.singleOrNull()?.toMap() ?: emptyMap(), it.second))
 }
 
 val usesDeclarationParser = takeRight(one { it.type == TokenType.USES }, nameParser.separator(commaParser))
--- src/main/kotlin/io/github/spacialcircumstances/efun/expressions/ObjectTypeExpression.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/expressions/ObjectTypeExpression.kt
@@ -2,12 +2,28 @@ package io.github.spacialcircumstances.efun.expressions
 
 import io.github.spacialcircumstances.efun.interpreter.*
 
-class ObjectTypeExpression(val params: Map<String, PlaceholderType>, val body: List<AbstractExpression>): AbstractTypeExpression() {
+class ObjectTypeExpression(val name: String, val params: Map<String, PlaceholderType>, val body: List<AbstractExpression>): AbstractTypeExpression() {
+    var type: DataStructureType? = null
+    var constructorType: FunctionType? = null
+    val constructorName = "new$name"
     override fun evaluate(context: InterpreterContext) {
+        //TODO: Create constructor
 
     }
 
     override fun type(context: TypesContext): DataStructureType {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val objectContext = TypesContext(null, context.defaultTypeMappings)
+        val resolvedParams = params.map { Pair(it.key, it.value.resolveType(context)) }
+        resolvedParams.forEach { (name, t) ->
+            objectContext.registerPrivateType(name, t)
+        }
+        body.forEach {
+            it.guessType(objectContext)
+        }
+        val dataStructureDefinition = DataStructureDefinition(objectContext)
+        type = DataStructureType(name, dataStructureDefinition)
+        constructorType = createFunctionType(resolvedParams.map { it.second }, type!!)
+        context.registerPublicType(constructorName, constructorType!!)
+        return type!!
     }
 }
