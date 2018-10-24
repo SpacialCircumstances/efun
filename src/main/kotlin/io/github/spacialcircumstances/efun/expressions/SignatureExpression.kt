c07437a17072860b07b5532ebf408f6b5baeeb9e
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-10-24 21:38:04 +0200

    Pass signature information to expression

--- src/main/kotlin/io/github/spacialcircumstances/efun/expressions/SignatureExpression.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/expressions/SignatureExpression.kt
@@ -2,9 +2,10 @@ package io.github.spacialcircumstances.efun.expressions
 
 import io.github.spacialcircumstances.efun.interpreter.DataStructureType
 import io.github.spacialcircumstances.efun.interpreter.InterpreterContext
+import io.github.spacialcircumstances.efun.interpreter.PlaceholderType
 import io.github.spacialcircumstances.efun.interpreter.TypesContext
 
-class SignatureExpression: AbstractTypeExpression() {
+class SignatureExpression(private val parents: List<PlaceholderType>, private val members: Map<String, PlaceholderType>): AbstractTypeExpression() {
     override fun type(context: TypesContext): DataStructureType {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
