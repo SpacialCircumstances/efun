b57e27aea335ea8857e54ffaaf7dea4788b83ecf
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-10-31 21:12:36 +0100

    Implement is expression

--- src/main/kotlin/io/github/spacialcircumstances/efun/expressions/IsExpression.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/expressions/IsExpression.kt
@@ -3,11 +3,16 @@ package io.github.spacialcircumstances.efun.expressions
 import io.github.spacialcircumstances.efun.interpreter.*
 
 class IsExpression(val expression: AbstractExpression, val typeExpr: PlaceholderType): AbstractExpression() {
+    var type: FType<*>? = null
     override fun evaluate(context: InterpreterContext): FValue {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val value = expression.evaluate(context)
+        val eq = value.type == type!!
+        return FValue(TBool, eq)
     }
 
     override fun guessType(context: TypesContext): FType<*> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        type = typeExpr.resolveType(context)
+        expression.guessType(context)
+        return TBool
     }
 }
