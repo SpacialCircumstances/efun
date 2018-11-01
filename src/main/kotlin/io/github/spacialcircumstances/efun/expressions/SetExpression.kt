c1ec7b29a3b72edd0ed5fd368b0738686483c469
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-01 17:27:07 +0100

    Implement set expression

--- src/main/kotlin/io/github/spacialcircumstances/efun/expressions/SetExpression.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/expressions/SetExpression.kt
@@ -1,18 +1,21 @@
 package io.github.spacialcircumstances.efun.expressions
 
+import io.github.spacialcircumstances.efun.TypeError
 import io.github.spacialcircumstances.efun.interpreter.*
 
 class SetExpression(private val expression: AbstractExpression, private val name: String): AbstractExpression() {
     override fun guessType(context: TypesContext): FType<*> {
-        TODO()
-        context.registerPublicType(name, expression.guessType(context))
+        val exprType = expression.guessType(context)
+        val varType = context.getType(name) ?: throw TypeError("Variable $name not found")
+        if (exprType != varType) {
+            throw TypeError("Expression type of ${exprType.name} does not match variable type of ${varType.name}")
+        }
         return TVoid
     }
 
     override fun evaluate(context: InterpreterContext): FValue {
-        TODO()
-        val result = expression.evaluate(context)
-        context[name] = result
-        return result
+        val value = expression.evaluate(context)
+        context.setMutable(name, value)
+        return FValue(TVoid, null)
     }
 }
