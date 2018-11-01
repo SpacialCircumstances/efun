0071952b25c4b120aa1f1c4e05cd97b518ebf53b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-01 21:40:43 +0100

    Begin support for setting fields in objects

--- src/main/kotlin/io/github/spacialcircumstances/efun/expressions/SetExpression.kt
+++ src/main/kotlin/io/github/spacialcircumstances/efun/expressions/SetExpression.kt
@@ -4,9 +4,11 @@ import io.github.spacialcircumstances.efun.TypeError
 import io.github.spacialcircumstances.efun.interpreter.*
 
 class SetExpression(private val expression: AbstractExpression, private val name: String): AbstractExpression() {
+    private val keys = name.split('.')
+
     override fun guessType(context: TypesContext): FType<*> {
         val exprType = expression.guessType(context)
-        val varType = context.getType(name) ?: throw TypeError("Variable $name not found")
+        val varType = getType(context, keys) ?: throw TypeError("Variable $name not found")
         if (exprType != varType) {
             throw TypeError("Expression type of ${exprType.name} does not match variable type of ${varType.name}")
         }
@@ -15,7 +17,7 @@ class SetExpression(private val expression: AbstractExpression, private val name
 
     override fun evaluate(context: InterpreterContext): FValue {
         val value = expression.evaluate(context)
-        context.setMutable(name, value)
+        context.setMutable(name, value) //TODO
         return FValue(TVoid, null)
     }
 }
